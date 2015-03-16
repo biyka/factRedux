f7813ac44efa71fafabb7ed4de92093911fce823
Ben Downe <ben@biyka.com>
2015-03-16 23:10:48 +0000

    Create all the basic components of the package.

---
+++ R/factRedux.R
@@ -0,0 +1,18 @@
+#' @author Ben Downe
+#' 
+#' @param factor
+#' 
+#' @title factRedux Function
+#' 
+#' @details Function to create an other category in functions where there are too many different levels to present cleanly
+#' 
+#' 
+#' 
+#' 
+
+factRedux <- function(data, factor = "") {
+  
+  
+  
+  
+}
